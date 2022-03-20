f1e87b5dcaf40a510a178ff314bc248e7c86c354
Anna Koehler <anna@foundrymakes.com>
2022-03-20 10:36:31 -0500

    121 find greatest distance/diff

---
+++ 121.buy-sell-stock.ts
@@ -0,0 +1,20 @@
+/*
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+*/
+function maxProfit(prices: number[]): number {
+    let maxProfit: number = 0
+    let minPrice: number = prices[0]
+    for (let i = 1; i < prices.length; i++) {
+        if(minPrice > prices[i]) {
+            minPrice = prices[i]
+        } else if ((prices[i] - minPrice) > maxProfit) {
+            maxProfit = prices[i] - minPrice
+        }
+    }
+    return maxProfit
+}
